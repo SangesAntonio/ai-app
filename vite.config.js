c89ca4f9c360e19ab11a84f1a8a9535966e0860a
sanges65 <sanges65@gmail.com>
2025-02-22 17:08:08 +0000

    solution new

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import {defineConfig, loadEnv} from 'vite'
-
-export default defineConfig(({command, mode}) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    define: {
-      'process.env.GOOGLE_API_KEY': JSON.stringify(env.GOOGLE_API_KEY),
-    },
-  }
-})
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public', // Assicurati che questa riga sia presente
+});
